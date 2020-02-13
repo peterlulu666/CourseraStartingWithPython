785bf2289694b2dc04ba2de8c595d4968abab3fe
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-02-13 18:39:59 -0500

    commit

--- CommentsWeek4.py
+++ CommentsWeek4.py
@@ -3,3 +3,25 @@
 # Describe what is going on in a sequence of code
 # Document who wrote the code
 # Turn off a line of code
+
+
+# Open a file
+def open_file():
+    file_name = input("Enter file: ")
+    paragraph = open(file_name, 'r')
+
+
+# Count letter frequency
+def letter_frequency(word):
+    counts = dict()
+    for letter in word:
+        counts[letter] = counts.get(letter, 0) + 1
+    return counts
+
+
+def main():
+    word = str("programming")
+    print(letter_frequency(word))
+
+
+main()
