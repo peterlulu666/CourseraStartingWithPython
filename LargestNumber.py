46cbc0f3175957c8c53b9d6c4e395c5364d66e9e
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-02-16 10:04:03 -0500

    commit

--- LargestNumber.py
+++ LargestNumber.py
@@ -1,9 +1,11 @@
 # Compare the current_largest_number with every number in the list
 num_list = [2, 6, 96, 45, 5]
 # Initialize the current_largest_number
-current_largest_number = -1
+current_largest_number = None
 # Compare number
 for num_in_list in num_list:
+    if current_largest_number is None:
+        current_largest_number = num_in_list
     print("Compare the current largest number " + str(current_largest_number) + " with the number in list " + str(
         num_in_list))
     if num_in_list > current_largest_number:
@@ -13,5 +15,7 @@ for num_in_list in num_list:
     print("The current largest number is " + str(current_largest_number))
 # Print the current_largest_number
 print("The largest number in the list is " + str(current_largest_number))
+
+
 
 
